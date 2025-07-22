75d9abed5ab7c00f0f1b39cc6a09086f64cc5c74
Marcelo Bettini <bettinimarcelo@gmail.com>
2025-07-21 22:27:32 -0300

    counter ZoneJS and Signals

---
+++ 02-basics/src/app/components/counter.component.ts
@@ -0,0 +1,38 @@
+import { Component, signal, WritableSignal, } from "@angular/core"
+@Component({
+    selector: 'app-counter',
+    imports: [],
+    templateUrl: './counter.component.html',
+    styleUrls: ['./counter.component.css']
+}
+)
+export class CounterComponent {
+    count: number = 0;
+    signalCount: WritableSignal<number> = signal(0);
+    public constructor() { }
+    //Using Zone JS
+    // This is the traditional way of handling state in Angular
+    // It uses Angular's change detection mechanism to update the view
+    increment() {
+        this.count++
+    }
+    decrement() {
+        this.count--
+    }
+    reset() {
+        this.count = 0
+    }
+
+    //Using Signals
+    // This is a new way of handling state in Angular
+    // It uses a reactive programming model to update the view
+    incrementSignal() {
+        this.signalCount.update(value => value + 1)
+    }
+    decrementSignal() {
+        this.signalCount.update(value => value - 1)
+    }
+    resetSignal() {
+        this.signalCount.set(0)
+    }
+}
